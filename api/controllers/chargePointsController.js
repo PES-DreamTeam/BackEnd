ada964c1a7fe44ad36fb228a88a7cbe614fa203f
Rashgaron <45242200+Rashgaron@users.noreply.github.com>
2022-03-10 19:28:09 +0100

    Added find charge points by id and cache

--- api/controllers/chargePointsController.js
+++ api/controllers/chargePointsController.js
@@ -2,7 +2,9 @@ const { chargePointService } = require('../services');
 
 const getAll = async (req, res) => {
     try {
-        const data = await chargePointService.get(req.query);
+        const data = await chargePointService.get();
+
+        if(!data) return res.status(404).send({msg: "ChargePoint not found"});
         res.status(200).send({chargePoints:data});       
     } catch (error) {
         console.log(error.toString());
@@ -10,8 +12,16 @@ const getAll = async (req, res) => {
     }
 }
 
-const getById = (req, res) => {
+const getById = async (req, res) => {
+    try {
+        const data = await chargePointService.get(req.params.id);
 
+        if(!data) return res.status(404).send({msg: "ChargePoint not found"});
+        res.status(200).send({chargePoint: data});
+    } catch (error) {
+       console.log(error.toString());
+       res.status(500).send({error: error.toString()});
+    }
 }
 
 module.exports = {
--- api/services/chargePoint.service.js
+++ api/services/chargePoint.service.js
@@ -1,25 +1,35 @@
 const axios = require('axios')
+const NodeCache = require('node-cache');
 
+const cache = new NodeCache({  stdTTL:600 });
 
-const get = async (query) => {
+const get = async (filter) => {
     try {
-        var response = await axios.get('https://api.bsmsa.eu/ext/api/bsm/chargepoints/states')
-        var data = response.data.map(item => {
-            return {
-                id: item.Station_id,
-                name: item.Station_name,
-                address: item.Station_address,
-                vehicle_type: item.Vehicle_type,
-                lat: item.Station_lat,
-                lng: item.Station_lng,
-                socket_data: {
-                    socket_id: item.Sockets[0].Connector_id,
-                    socket_type: item.Sockets[0].Connector_types,
-                    charge_modes: item.Sockets[0].Charge_modes,
-                    socket_state: item.Sockets[0].State,
+        var data = cache.get('chargePoints');
+
+        if(!data) {
+            var response = await axios.get('https://api.bsmsa.eu/ext/api/bsm/chargepoints/states')
+            data = response.data.map(item => {
+                return {
+                    id: item.Station_id,
+                    name: item.Station_name,
+                    address: item.Station_address,
+                    vehicle_type: item.Vehicle_type,
+                    lat: item.Station_lat,
+                    lng: item.Station_lng,
+                    socket_data: {
+                        socket_id: item.Sockets[0].Connector_id,
+                        socket_type: item.Sockets[0].Connector_types,
+                        charge_modes: item.Sockets[0].Charge_modes,
+                        socket_state: item.Sockets[0].State,
+                    }
                 }
-            }
-        });
+            });
+            cache.set('chargePoints', data, 600);
+        }
+
+        if(filter) data = data.filter(item => item.id = filter);
+
     return data;
 
     } catch (error) {
