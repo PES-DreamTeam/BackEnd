b5d134ff83c35b0f28c08988037a38a3981f5b4b
Rashgaron <45242200+Rashgaron@users.noreply.github.com>
2022-03-19 23:33:23 +0100

    Return user on login and register

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -5,11 +5,12 @@ const register = async (req, res) => {
         const user = await userService.getByEmail(req.body.email);
         if(user) return res.status(409).send({error: "User already exists"});
 
-        const newUser = await authService.register(req.body);
-        await userService.create(newUser);
+        let newUser = await authService.register(req.body);
+        newUser = await userService.create(newUser);
+        newUser = await userService.feedUserToWeb(newUser);
         const token = await authService.signToken(newUser._id);
 
-        return res.status(201).send({token});
+        return res.status(201).send({token, user: newUser});
     } catch (error) {
         return res.status(500).send({msg: error.toString()})  
     }
@@ -22,7 +23,7 @@ const login = async (req, res) => {
 
         const token = await authService.login(user, req.body);
         if(token){
-            return res.status(200).send({ token });
+            return res.status(200).send({ token, user: await userService.feedUserToWeb(user)});
         }else {
             return res.status(403).send({Error: 'Invalid credentials'});
         }
