65e28880e28d46660f251f220ed8637b84fb7bc9
AwaKT <gerardm27@gmail.com>
2022-04-21 17:53:44 +0200

    TG-131 Endpoint actualizado OTRA VEZ

--- api/services/chargePoint.service.js
+++ api/services/chargePoint.service.js
@@ -170,6 +170,12 @@ const chargePointService = (dependencies) => {
         return station;
     }
 
+    const getReports = async (id) => {
+        const station = await ReportStations.findOne({station_id: id});
+        console.log(station);
+        return station.reports;
+    }
+
     const feedStationToWeb = async (station) => {
         const defaultStation = await DefaultStations.findOne({station_id: station.station_id});
         return {
@@ -190,7 +196,8 @@ const chargePointService = (dependencies) => {
         voteStation,
         reportStation,
         feedStationToWeb,
-        getChargePointsById
+        getChargePointsById,
+        getReports
     }
 }
 
